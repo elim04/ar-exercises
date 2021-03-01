b5f5b39561aa0732a65b91d2545a16c438a484c2
Emily Lim <emily.elim04@gmail.com>
2021-03-01 20:37:47 +0000

    where method on stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,26 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+
+@mens_stores = Store.where(mens_apparel: true)
+
+#loop through stores to share name and annual revenue
+@mens_stores.each do |store| 
+  puts "#{store.name} carries mens apparel and has an annual revenue of #{store.annual_revenue}."
+end
+
+#can write where statement similar to SQL statment in quotes, use question marks to prevent injection risk
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000) 
+
+@womens_stores.each do |store| 
+  puts "#{store.name} carries womens apparel and has an annual revenue of #{store.annual_revenue}."
+end
+
+
