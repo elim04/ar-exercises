714ab9f84410a8b65423598123cacedc7e6cf643
Emily Lim <emily.elim04@gmail.com>
2021-03-01 21:49:26 +0000

    add validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Hi, please enter new store name."
+store_name = gets.chomp
+
+@new_store = Store.create(name: store_name)
+
+@new_store.valid?
+
+@new_store.errors.full_messages.each do |message|
+  puts message
+end
+
+
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  validate :must_carry_one_apparel 
+
+  def must_carry_one_apparel
+    if !self.womens_apparel && !self.mens_apparel 
+      errors.add(:mens_apparel, "Need to input one type of apparel")
+      errors.add(:womens_apparel, "Need to input one type of apparel")
+    end
+  end
+
 end
